7ddb56070767c5c9bc6c882fe8c54427ece97591
haimra <haimra@gmail.com>
2014-05-19 16:24:14 +0300

    Adding JUnits infra

--- src/main/java/org/ramanh/controller/UserController.java
+++ src/main/java/org/ramanh/controller/UserController.java
@@ -9,11 +9,12 @@ import javax.annotation.PostConstruct;
 import org.ramanh.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,7 +36,8 @@ public class UserController {
 		return usersMap.get(id);
 	}
 	
-	@RequestMapping(value = "/users", method = RequestMethod.POST )
+	@RequestMapping(value = "/users", method = RequestMethod.POST)
+	@ResponseStatus(value=HttpStatus.CREATED,reason="New Object created")	
 	public void addUser(@RequestBody User user){
 		usersMap.put(user.getId(), user);		
 	}
